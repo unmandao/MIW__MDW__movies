de1edbe826d1f977aa82f6f393d0e7b87072a36c
David Bausela <david.bausela@alumnos.upm.es>
2023-03-29 20:40:53 +0200

    v15 Smell Code: Sentencia Alternativa Múltiple (Clase “Movie” - método “getCharge()”) => el comportamiento depende de un tipo/código/... Refactoring Reemplazar Código de Tipo con Estrategia/Estado (inyección de dependencias) => Mover método: método “getCharge()” a Clase “Price”

--- src/main/java/usantatecla/movies/Movie.java
+++ src/main/java/usantatecla/movies/Movie.java
@@ -15,25 +15,7 @@ public class Movie {
 	}
 	
 	public double getCharge(int daysRented) {
-		double result = 0;
-		switch (this.getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (daysRented > 2) {
-				result += (daysRented - 2) * 1.5;
-			}
-			break;
-		case Movie.NEW_RELEASE:
-			result += 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (daysRented > 3) {
-				result += (daysRented - 1) * 1.5;
-			}
-			break;
-		}
-		return result;
+		return price.getCharge(daysRented);
 	}
 
 	public int getFrequentRenterPoints(int daysRented) {
