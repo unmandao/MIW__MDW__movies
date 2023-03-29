8b3c60c9dd29f4b45e5b398af8f3f7f0074a0832
David Bausela <david.bausela@alumnos.upm.es>
2023-03-29 20:48:51 +0200

    v16 Smell Code: Sentencia Alternativa Múltiple (Clase “Price” - método “getCharge()”) => el comportamiento depende de un tipo/código/... Refactoring Reemplazar Condicional con Polimorfismo => Redefinir método “getCharge()” en clases derivadas

--- src/main/java/usantatecla/movies/ChildrenPrice.java
+++ src/main/java/usantatecla/movies/ChildrenPrice.java
@@ -6,5 +6,14 @@ public class ChildrenPrice extends Price {
 	int getPriceCode() {
 		return Movie.CHILDRENS;
 	}
+	
+	@Override
+	public double getCharge(int daysRented) {
+		double result = 1.5;
+		if (daysRented > 3) {
+			result += (daysRented - 1) * 1.5;
+		}
+		return result;
+	}
 
 }
--- src/main/java/usantatecla/movies/NewReleasePrice.java
+++ src/main/java/usantatecla/movies/NewReleasePrice.java
@@ -6,5 +6,10 @@ public class NewReleasePrice extends Price {
 	int getPriceCode() {
 		return Movie.NEW_RELEASE;
 	}
+	
+	@Override
+	public double getCharge(int daysRented) {
+		return 3;
+	}
 
 }
