170d42d2e80ee70ce06d1d99f0d898e6dda6fece
David Bausela <dbausela@gmail.com>
2023-03-29 14:29:20 +0200

    v10 Smell Code: Experto en la Información (Clase “Rental“ – Método “getCharge()“) => la mayor parte de la información es de Movie Refactoring Mover Método => método “getCharge()” a clase “Movie”

--- src/main/java/usantatecla/movies/Movie.java
+++ src/main/java/usantatecla/movies/Movie.java
@@ -14,6 +14,28 @@ public class Movie {
 		this.priceCode = priceCode;
 	}
 	
+	public double getCharge(int daysRented) {
+		double result = 0;
+		switch (priceCode) {
+		case Movie.REGULAR:
+			result += 2;
+			if (daysRented > 2) {
+				result += (daysRented - 2) * 1.5;
+			}
+			break;
+		case Movie.NEW_RELEASE:
+			result += 3;
+			break;
+		case Movie.CHILDRENS:
+			result += 1.5;
+			if (daysRented > 3) {
+				result += (daysRented - 1) * 1.5;
+			}
+			break;
+		}
+		return result;
+	}
+	
 	public int getPriceCode() {
 		return priceCode;
 	}
--- src/main/java/usantatecla/movies/Rental.java
+++ src/main/java/usantatecla/movies/Rental.java
@@ -11,25 +11,7 @@ public class Rental {
 	}
 	
 	public double getCharge() {
-		double result = 0;
-		switch (this.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (this.getDaysRented() > 2) {
-				result += (this.getDaysRented() - 2) * 1.5;
-			}
-			break;
-		case Movie.NEW_RELEASE:
-			result += 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (this.getDaysRented() > 3) {
-				result += (this.getDaysRented() - 1) * 1.5;
-			}
-			break;
-		}
-		return result;
+		return movie.getCharge(daysRented);
 	}
 	
 	public int getFrequentRenterPoints() {
